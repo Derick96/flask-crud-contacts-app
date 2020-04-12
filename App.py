f454d19f702bb4ead3a56651873a2c123ee2c21f
Derick96 <60660484+Derick96@users.noreply.github.com>
2020-04-11 20:10:39 -0600

    nombre de tabla

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost' 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'flaskcrud'
+app.config['MYSQL_DB'] = 'flaskcontacts'
 mysql = MySQL(app)
 
 # settings
